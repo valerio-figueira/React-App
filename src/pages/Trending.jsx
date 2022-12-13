a9dd5a8bd406d00748b01a2b50a16e3e02ff44e6
valerio-figueira <j.valerio.figueira@gmail.com>
2022-12-12 21:49:21 -0300

    new code

--- src/pages/Trending.jsx
+++ src/pages/Trending.jsx
@@ -1,15 +1,41 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
   // CSS
   import './Trending.css';
 
   // COMPONENTS
-  import LoadingShimmerLine from '../components/LoadingShimmerLine';
+  import LoadingShimmer from '../components/LoadingShimmer';
+
+  // HOOKS
+  import FetchAPI from '../hooks/FetchAPI';
+
+  const URL = "https://resilient-kangaroo-970dc9.netlify.app/popular";
 
 const Trending = () => {
+  const posts = FetchAPI(URL);
+
   return (
-    <div>
-      <LoadingShimmerLine />
+    <div className='Trending'>
+      {posts ? (
+        posts.map(post => {
+          return (
+            <article className='post' key={post._id}>
+              <Link to={`/Popular/${post._id}`}>
+                <h2 className='title'>{post.title}</h2>
+              </Link>
+              <Link to={`/Popular/${post._id}`}>
+                <img src={post.image[0]} alt={post.title} />
+              </Link>
+              <Link to={`/Popular/${post._id}`}>
+                <p className='description'>{post.description}</p>
+              </Link>
+            </article>
+          )
+        })
+      ) : (
+        <LoadingShimmer />
+      )}
     </div>
   )
 }
